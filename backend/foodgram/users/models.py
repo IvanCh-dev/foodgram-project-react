e0a2418aa27d1b9d4a64b74a78d0c51ef35539c8
Ivan Chuzhba <ivan94-20@yandex.ru>
2023-03-23 17:06:58 +0600

    Fixed users.models

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -19,10 +19,12 @@ class User(AbstractUser):
     first_name = models.CharField(
         max_length=150,
         verbose_name='Имя',
+        null=True,
     )
     last_name = models.CharField(
         max_length=150,
         verbose_name='Фамилия',
+        null=True,
     )
 
     class Meta:
