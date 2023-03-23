a37fa67380b8bebe48b35111aad8f033d027389d
Ivan Chuzhba <ivan94-20@yandex.ru>
2023-03-23 22:20:03 +0600

    Fixed command

--- backend/foodgram/recipes/management/commands/get_ingr_from_json.py
+++ backend/foodgram/recipes/management/commands/get_ingr_from_json.py
@@ -2,6 +2,7 @@ import json
 
 from django.core.management.base import BaseCommand
 
+from foodgram import settings
 from recipes.models import Ingredient
 
 
@@ -9,8 +10,8 @@ class Command(BaseCommand):
     print('Начало загрузки')
 
     def handle(self, *args, **options):
-        with open('D:\\Dev\\foodgram-project-react\\data\\ingredients.json',
-                  'r', encoding='utf-8') as f:
+        path = f'{settings.BASE_DIR}/data/ingredients.json'
+        with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
         for ingredient in data:
             Ingredient.objects.get_or_create(
