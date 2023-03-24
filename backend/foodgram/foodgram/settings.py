c8a6b0921960fb1f00ac0affde7c2e98f5e88e1f
Ivan Chuzhba <ivan94-20@yandex.ru>
2023-03-24 19:16:53 +0600

    Fixed infra files

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/app/static/'
 
 AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
