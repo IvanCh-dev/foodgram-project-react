f84d6a3c78d53b4540aa2212991101d32fee013d
Ivan Chuzhba <ivan94-20@yandex.ru>
2023-03-26 18:01:07 +0600

    Fixed settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -107,7 +107,8 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+        'PORT': os.getenv('DB_PORT', default='5432'),
+        'ATOMIC_REQUESTS': True
     }
 }
 
